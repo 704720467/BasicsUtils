6fce3608e984cf44d36e423735209fec1fb19b3a
Zhangpeng <704720467@qq.com>
2018-12-05 11:23:33 +0800

    添加类库

---
+++ baseutilslib/src/main/java/cn/zp/baseutilslib/MyUtils.java
@@ -0,0 +1,12 @@
+package cn.zp.baseutilslib;
+
+/**
+ * 测试类
+ * Created by zp on 2018/12/5.
+ */
+
+public class MyUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
